18de2355b6e5d9c6b4e2d272f1a1cccf869a74ff
Veysel Kartalmis <veyselkartalms@gmail.com>
2022-05-30 18:52:51 +0300

    sidebar added

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,13 @@
+import Sidebar from "../../components/sidebar/Sidebar";
 import "./home.scss";
 
 const Home = () => {
-    return (
-        <div className="home">Home</div>
-    )
-}
+  return (
+    <div className="home">
+      <Sidebar />
+      <div className="homeContainer"></div>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
