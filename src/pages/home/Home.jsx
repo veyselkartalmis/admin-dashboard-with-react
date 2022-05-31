80d53fa90d17ea60f77a5bea3e2568980c6243a0
Veysel <veyselkartalms@gmail.com>
2022-05-31 20:52:44 +0300

    mui table added

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,6 +3,7 @@ import Featured from "../../components/featured/Featured";
 import Navbar from "../../components/navbar/Navbar";
 import Sidebar from "../../components/sidebar/Sidebar";
 import Widget from "../../components/widget/Widget";
+import List from "../../components/table/List";
 import "./home.scss";
 
 const Home = () => {
@@ -20,6 +21,10 @@ const Home = () => {
 				<div className="charts">
 					<Featured />
 					<Chart />
+				</div>
+				<div className="listContainer">
+					<div className="listTitle">Latest Transactions</div>
+					<List />
 				</div>
 			</div>
 		</div>
