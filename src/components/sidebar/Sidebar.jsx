6effc4e2bd4234c9781f596cf7f409a9fc9021cb
Veysel Kartalmis <veyselkartalms@gmail.com>
2022-05-31 12:23:37 +0300

    navbar added

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -13,75 +13,75 @@ import PsychologyOutlinedIcon from "@mui/icons-material/PsychologyOutlined";
 import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined";
 
 const Sidebar = () => {
-  return (
-    <div className="sidebar">
-      <div className="top">
-        <span className="logo">lamadmin</span>
-      </div>
-      <hr />
-      <div className="center">
-        <ul>
-          <p className="title">MAIN</p>
-          <li>
-            <DashboardIcon className="icon" />
-            <span>Dashboard</span>
-          </li>
-          <p className="title">LIST</p>
-          <li>
-            <PersonOutlineIcon className="icon" />
-            <span>Users</span>
-          </li>
-          <li>
-            <StoreIcon className="icon" />
-            <span>Products</span>
-          </li>
-          <li>
-            <CreditCardIcon className="icon" />
-            <span>Orders</span>
-          </li>
-          <li>
-            <LocalShippingIcon className="icon" />
-            <span>Delivery</span>
-          </li>
-          <p className="title">USEFUL</p>
-          <li>
-            <InsertChartIcon className="icon" />
-            <span>Stats</span>
-          </li>
-          <li>
-            <NotificationsNoneIcon className="icon" />
-            <span>Notifications</span>
-          </li>
-          <p className="title">SERVICE</p>
-          <li>
-            <SettingsSystemDaydreamOutlinedIcon className="icon" />
-            <span>System Health</span>
-          </li>
-          <li>
-            <PsychologyOutlinedIcon className="icon" />
-            <span>Logs</span>
-          </li>
-          <li>
-            <SettingsApplicationsIcon className="icon" />
-            <span>Settings</span>
-          </li>
-          <p className="title">USER</p>
-          <li>
-            <AccountCircleOutlinedIcon className="icon" />
-            <span>Profile</span>
-          </li>
-          <li>
-            <ExitToAppIcon className="icon" />
-            <span>Logout</span>
-          </li>
-        </ul>
-      </div>
-      <div className="bottom">
-          <div className="colorOption"></div>
-          <div className="colorOption"></div>
-      </div>
-    </div>
-  );
+	return (
+		<div className="sidebar">
+			<div className="top">
+				<span className="logo">lamadmin</span>
+			</div>
+			<hr />
+			<div className="center">
+				<ul>
+					<p className="title">MAIN</p>
+					<li>
+						<DashboardIcon className="icon" />
+						<span>Dashboard</span>
+					</li>
+					<p className="title">LIST</p>
+					<li>
+						<PersonOutlineIcon className="icon" />
+						<span>Users</span>
+					</li>
+					<li>
+						<StoreIcon className="icon" />
+						<span>Products</span>
+					</li>
+					<li>
+						<CreditCardIcon className="icon" />
+						<span>Orders</span>
+					</li>
+					<li>
+						<LocalShippingIcon className="icon" />
+						<span>Delivery</span>
+					</li>
+					<p className="title">USEFUL</p>
+					<li>
+						<InsertChartIcon className="icon" />
+						<span>Stats</span>
+					</li>
+					<li>
+						<NotificationsNoneIcon className="icon" />
+						<span>Notifications</span>
+					</li>
+					<p className="title">SERVICE</p>
+					<li>
+						<SettingsSystemDaydreamOutlinedIcon className="icon" />
+						<span>System Health</span>
+					</li>
+					<li>
+						<PsychologyOutlinedIcon className="icon" />
+						<span>Logs</span>
+					</li>
+					<li>
+						<SettingsApplicationsIcon className="icon" />
+						<span>Settings</span>
+					</li>
+					<p className="title">USER</p>
+					<li>
+						<AccountCircleOutlinedIcon className="icon" />
+						<span>Profile</span>
+					</li>
+					<li>
+						<ExitToAppIcon className="icon" />
+						<span>Logout</span>
+					</li>
+				</ul>
+			</div>
+			<div className="bottom">
+				<div className="colorOption"></div>
+				<div className="colorOption"></div>
+			</div>
+		</div>
+	);
 };
 
 export default Sidebar;
