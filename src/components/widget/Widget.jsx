dd0a99acc93ff247ecd6badff89fd86959595442
Veysel Kartalmis <veyselkartalms@gmail.com>
2022-05-31 14:05:42 +0300

    widget added++

---
+++ src/components/widget/Widget.jsx
@@ -0,0 +1,27 @@
+import "./widget.scss";
+import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
+import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
+import AccountBalanceWalletOutlinedIcon from "@mui/icons-material/AccountBalanceWalletOutlined";
+import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
+import MonetizationOnOutlinedIcon from "@mui/icons-material/MonetizationOnOutlined";
+
+const Widget = () => {
+	return (
+		<div className="widget">
+			<div className="left">
+				<span className="title">USERS</span>
+				<span className="counter">1947</span>
+				<span className="link">See All User</span>
+			</div>
+			<div className="right">
+				<div className="percentage negative">
+					<KeyboardArrowUpIcon />
+					20%
+				</div>
+				<PersonOutlinedIcon className="icon" />
+			</div>
+		</div>
+	);
+};
+
+export default Widget;
